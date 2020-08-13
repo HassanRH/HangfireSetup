a3a358644b0f1e828da7935b204a9c6540d283b4
HassanRH <hassan---06@hotmail.com>
2020-08-13 13:05:59 +0200

    latest file

---
+++ HangfireSetup/HangfireDemo.Core/Demo/DemoService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HangfireDemo.Core.Demo
+{
+    public interface IDemoService
+    {
+        void RunDemoTask();
+    }
+
+    public class DemoService : IDemoService
+    {
+       public void RunDemoTask()
+        {
+            Console.WriteLine("Denne task er kørt fra demo service");
+        }
+
+    }
+}
---
+++ HangfireSetup/HangfireSetup/Controllers/HangFireController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using HangfireDemo.Core.Demo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangfireSetup.Controllers
+{
+    [Route("/Api/HangFire")]
+
+    public class HangFireController : Controller
+    {
+        private IDemoService demoService;
+        public HangFireController(IDemoService demoService)
+        {
+            this.demoService = demoService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            RecurringJob.AddOrUpdate("demo-job",() => this.demoService.RunDemoTask(),
+            Cron.Minutely);
+            this.demoService.RunDemoTask();
+
+            return Ok();
+        }
+    }
+}
